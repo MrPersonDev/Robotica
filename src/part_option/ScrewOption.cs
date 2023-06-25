64080e9099deb79eafa47e513fab293ab707dceb
MrPersonDev <simonashton37@gmail.com>
2023-06-25 10:15:17 -0700

    Add colored screws

--- src/part_option/ScrewOption.cs
+++ src/part_option/ScrewOption.cs
@@ -8,10 +8,13 @@ public partial class ScrewOption : PartOption
     private readonly float[] SIZES = { 0.25f, 0.375f, 0.5f, 0.625f, 0.75f, 0.875f, 1.0f, 1.25f, 1.5f, 1.75f, 2.0f, 2.25f, 2.5f };
 
     private Dictionary<float, PartObject> partObjects = new Dictionary<float, PartObject>();
+    private Dictionary<float, PartObject> coloredPartObjects = new Dictionary<float, PartObject>();
 
     [ExportGroup("Part Objects")]
     [Export]
     private Godot.Collections.Array<PackedScene> partScenes = new Godot.Collections.Array<PackedScene>();
+    [Export]
+    private Godot.Collections.Array<PackedScene> coloredPartScenes = new Godot.Collections.Array<PackedScene>();
 
     public override void _Ready()
     {
@@ -26,14 +29,20 @@ public partial class ScrewOption : PartOption
         {
             PartObject curPartObject = new PartObject(partScenes[i]);
             partObjects[SIZES[i]] = curPartObject;
+            
+            PartObject curColoredPartObject = new PartObject(coloredPartScenes[i]);
+            coloredPartObjects[SIZES[i]] = curColoredPartObject;
         }
     }
 
     public override PartObject GetPartObject(Dictionary<String, Variant> parameters)
     {
         float length = float.Parse((String)parameters["Length"]);
-
-        return partObjects[length];
+        
+        if ((bool)parameters["Colored"])
+            return coloredPartObjects[length];
+        else
+            return partObjects[length];
     }
 
     public override void Setup(Part part, Dictionary<String, Variant> parameters)
@@ -49,6 +58,7 @@ public partial class ScrewOption : PartOption
         List<Object> lengthsObjectList = lengths.Cast<Object>().ToList();
 
         parameterTypes.Add(Tuple.Create("Length", (ParameterType)new DropdownParameter(lengthsObjectList)));
+        parameterTypes.Add(Tuple.Create("Colored", (ParameterType)new BooleanParameter(false)));
 
         return parameterTypes;
     }
@@ -58,6 +68,9 @@ public partial class ScrewOption : PartOption
         List<PartObject> partObjectsList = new List<PartObject>();
 
         foreach (PartObject partObject in partObjects.Values)
+            partObjectsList.Add(partObject);
+        
+        foreach (PartObject partObject in coloredPartObjects.Values)
             partObjectsList.Add(partObject);
 
         return partObjectsList;
